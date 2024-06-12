f88b2f113df3261bf9cab23b4e03f5f4e72d157c
Mateus de Almeida <80906504+Mateuszinnn@users.noreply.github.com>
2024-06-11 21:21:39 -0300

    bottomsheet comment

--- lib/components/post_widgets/post_interaction_button.dart
+++ lib/components/post_widgets/post_interaction_button.dart
@@ -26,7 +26,73 @@ class PostInteractionButton extends StatelessWidget {
           ),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              backgroundColor: Colors.transparent,
+              isDismissible: true,
+              builder: (BuildContext context) {
+                return GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () => Navigator.pop(context),
+                  child: Stack(
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: DraggableScrollableSheet(
+                          initialChildSize: 1.0,
+                          minChildSize: 0.25,
+                          maxChildSize: 1.0,
+                          expand: true,
+                          builder: (context, scrollController) {
+                            return Container(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSecondaryContainer,
+                              child: SingleChildScrollView(
+                                controller: scrollController,
+                                child: Column(
+                                  children: <Widget>[
+                                    const SizedBox(height: 10),
+                                    Container(
+                                      width: 50,
+                                      height: 5,
+                                      decoration: BoxDecoration(
+                                        color: Colors.grey,
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                    ),
+                                    const Text(
+                                      'Comentários',
+                                      style: TextStyle(fontFamily: 'Instagram', fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(height: 20),
+                                    const Text(
+                                      'Este é um Bottom Sheet que vai até o topo!',
+                                      style: TextStyle(fontSize: 60),
+                                    ),
+                                    const Text(
+                                      'Este é um Bottom Sheet que vai até o topo!',
+                                      style: TextStyle(fontSize: 60),
+                                    ),
+                                    const Text(
+                                      'Este é um Bottom Sheet que vai até o topo!',
+                                      style: TextStyle(fontSize: 60),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
           icon: const Icon(Icons.chat_bubble_outline),
         ),
         IconButton(
